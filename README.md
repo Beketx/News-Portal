6f69a6a55a63971ea2658ed540bdf0d6424a3477
beketx <beketsk@gmail.com>
2020-10-22 13:11:05 +0600

    Form2.0

--- README.md
+++ README.md
@@ -12,3 +12,24 @@ form.as_p as_ul as_table
 News.objects.create(**form.cleaned_data)
 form = NewForm(request.POST) reques.Post это данные из браузера
 
+22/2020
+Forms 2.0
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        print(context)
+        context['title'] = 'Главная страница'
+        return context
+
+
+class HomeNews(ListView):
+    model = News
+    template_name = 'news/home_news_list.html'
+    context_object_name = 'news'
+    extra_context = {'title': 'Главная'}
+
+  get_queryset
+
+  def clean_title:
+
+  re.match(pattern, string)
--- portal/news/views.py
+++ portal/news/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from news.forms import NewsForm
-
+from django.views.generic import ListView
 from .models import News, Category
+
+class HomeNews(ListView):
+    model = News
+    template_name = 'news/home_news_list.html'
+    context_object_name = 'news'
+    extra_context = {'title': 'Главная'}
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        print(context)
+        context['title'] = 'Главная страница'
+        return context
+
+    def get_queryset(self):
+        return News.objects.filter(is_published=True)
 
 def index(request):
     news = News.objects.all()
@@ -26,8 +41,9 @@ def add_news(request):
     if request.method == 'POST':
         form = NewsForm(request.POST)
         if form.is_valid():
-            # print(form.cleaned_data)
-            news = News.objects.create(**form.cleaned_data)
+            # # print(form.cleaned_data)
+            # news = News.objects.create(**form.cleaned_data)
+            news = form.save()
             return redirect(news)
     else:
         form = NewsForm()
